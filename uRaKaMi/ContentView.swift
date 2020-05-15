6538e944ba52d05edb097b23f050df941adc4f3b
shi-n <shi-n_xyz@cloudsquare.jp>
2020-05-15 20:29:48 +0900

    Initial Commit

---
+++ uRaKaMi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  uRaKaMi
+//
+//  Created by Shin Inaba on 2020/05/15.
+//  Copyright © 2020 shi-n. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
